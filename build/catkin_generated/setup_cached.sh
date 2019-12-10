b2afef46d33e9755fd909ccf55dd974d8138fe6e
LIVERPOOL1892 <walter-frank@artc.a-star.edu.sg>
2019-12-10 08:49:09 +0700

    move_base_simple target updated

--- build/catkin_generated/setup_cached.sh
+++ build/catkin_generated/setup_cached.sh
@@ -7,6 +7,4 @@
 # new environment variables
 
 # modified environment variables
-export LD_LIBRARY_PATH="/home/frank/Documents/ws_sensor_bosch/CISS_BOSCH_INTEGRATION/ciss_ros_wrapper/devel/lib:/opt/ros/melodic/lib"
-export PKG_CONFIG_PATH="/home/frank/Documents/ws_sensor_bosch/CISS_BOSCH_INTEGRATION/ciss_ros_wrapper/devel/lib/pkgconfig:/opt/ros/melodic/lib/pkgconfig"
 export PWD="/home/frank/Documents/ws_tester/urdf_tester/build"

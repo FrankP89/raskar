8b2964b75b506f965360823043249d9d680d2497
LIVERPOOL1892 <walter.pintor@gmx.com>
2019-12-20 17:36:42 +0800

    Uploading to fix

---
+++ build/catkin_generated/order_packages.py
@@ -0,0 +1,5 @@
+# generated from catkin/cmake/template/order_packages.context.py.in
+source_root_dir = "/home/frank/ws_raskar_urdf/src"
+whitelisted_packages = "".split(';') if "" != "" else []
+blacklisted_packages = "".split(';') if "" != "" else []
+underlay_workspaces = "/home/frank/ws_tester_urdf/devel;/opt/ros/kinetic".split(';') if "/home/frank/ws_tester_urdf/devel;/opt/ros/kinetic" != "" else []
---
+++ build/raskar_urdf/CTestTestfile.cmake
@@ -0,0 +1,6 @@
+# CMake generated Testfile for 
+# Source directory: /home/frank/ws_raskar_urdf/src/raskar_urdf
+# Build directory: /home/frank/ws_raskar_urdf/build/raskar_urdf
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.

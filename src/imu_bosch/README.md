dbd5a2c23067cbb5c372f16955d42a8ce26cdb4d
FrankP89 <walter.pintor@gmx.com>
2020-01-12 17:32:45 +0800

    Removing submodule

---
+++ src/imu_bosch/README.md
@@ -0,0 +1,27 @@
+# CISS_BOSCH_PORT_PYTHON
+
+Frank Pintor
+
+10 - 11 - 2019
+
+Allows the utilization of the CISS BOSCH sensor in ROS environments using Python 2.
+
+
+
+*Future work*
+
+- Correct description for the covariance matrices for IMU
+- Correct dimension and sizing of sensor
+
+
+*ROS Node*
+
+
+-    |/|/|/|/|/|/|__| 80%
+
+
+*Notes*
+
+- Python 3 can not be utilized correctly in this phase due to TF pkg limitation.
+- Pressure and noise sensors are not integrated in RViz
+- Noise sensor only works over bluetooth
---
+++ src/imu_bosch/ciss_ros_wrapper/build/catkin_generated/setup_cached.sh
@@ -0,0 +1,13 @@
+#!/usr/bin/env sh
+# generated from catkin/python/catkin/environment_cache.py
+
+# based on a snapshot of the environment before and after calling the setup script
+# it emulates the modifications of the setup script without recurring computations
+
+# new environment variables
+
+# modified environment variables
+export CMAKE_PREFIX_PATH="/home/frank/Documents/ws_sensor_bosch/CISS_BOSCH_INTEGRATION/ciss_ros_wrapper/devel:$CMAKE_PREFIX_PATH"
+export PWD="/home/frank/Documents/ws_sensor_bosch/CISS_BOSCH_INTEGRATION/ciss_ros_wrapper/build"
+export ROSLISP_PACKAGE_DIRECTORIES="/home/frank/Documents/ws_sensor_bosch/CISS_BOSCH_INTEGRATION/ciss_ros_wrapper/devel/share/common-lisp"
+export ROS_PACKAGE_PATH="/home/frank/Documents/ws_sensor_bosch/CISS_BOSCH_INTEGRATION/ciss_ros_wrapper/src:$ROS_PACKAGE_PATH"

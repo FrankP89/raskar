ba28fb905f03c58944090d737601ded84e60fd4b
FrankP89 <walter.pintor@gmx.com>
2020-01-16 01:08:16 +0800

    Adding initial navigation files and fake laser sensor information from gazebo

--- build/CTestTestfile.cmake
+++ build/CTestTestfile.cmake
@@ -5,6 +5,7 @@
 # This file includes the relevant testing commands required for 
 # testing this directory and lists subdirectories to be tested as well.
 subdirs("gtest")
+subdirs("raskar_navigation")
 subdirs("ultrasound/rosserial")
 subdirs("ultrasound/rosserial_arduino")
 subdirs("ultrasound/rosserial_mbed")

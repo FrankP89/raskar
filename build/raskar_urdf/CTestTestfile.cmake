985baece6a5d11e6c4c36f18f6a5c8fc472ee21a
Frank Pintor <walter.pintor@gmx.com>
2020-10-29 15:21:01 +0800

    Organizing structure of project
    Created folders to organize and document better

--- build/raskar_urdf/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/frank/ws_raskar_urdf/src/raskar_urdf
-# Build directory: /home/frank/ws_raskar_urdf/build/raskar_urdf
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
